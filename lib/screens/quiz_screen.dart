799cf915469440634f699c1b65e3b0697aca38ba
Your Name <ungefuk.vlad@mail.ru>
2025-05-28 01:55:55 +0300

    continue button and save progress game

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:audioplayers/audioplayers.dart';
 import '../models/quiz_question.dart';
-import '../button_style.dart';
 
 
 class QuizScreen extends StatefulWidget {
@@ -21,6 +22,8 @@ class _QuizScreenState extends State<QuizScreen> {
   bool answered = false;
   int? selectedAnswer;
   final player = AudioPlayer();
+  String selectedEra = '';
+  String selectedHistoryType = '';
 
   @override
   void initState() {
@@ -28,22 +31,45 @@ class _QuizScreenState extends State<QuizScreen> {
     WidgetsBinding.instance.addPostFrameCallback((_) => _initQuestions());
   }
 
-  void _initQuestions() {
+  Future<List<QuizQuestion>> loadQuizQuestions() async {
+    final String data  = await rootBundle.loadString('assets/data/quiz_questions.json');
+    final List<dynamic> jsonList = jsonDecode(data);
+    return jsonList.map((e) => QuizQuestion.fromJson(e)).toList();
+  }
+
+  Future<void> _initQuestions() async{
     final args = ModalRoute.of(context)!.settings.arguments as Map?;
-    final String era = args?['era'] ?? '';
-    final String historyType = args?['historyType'] ?? '';
+    selectedEra = args?['era'] ?? '';
+    selectedHistoryType = args?['historyType'] ?? '';
+    final bool resume = args?['resume'] ?? false;
+    final int savedIndex = args?['index'] ?? 0;
+
+    final allQuestions = await loadQuizQuestions();
 
     filteredQuestions = allQuestions
-    .where((q) => q.era == era && q.historyType == historyType)
+    .where((q) => q.era == selectedEra && q.historyType == selectedHistoryType)
     .toList();
 
     if(filteredQuestions.isEmpty) {
       _showEmptyDialog();
     } else {
-      _loadBestScore();
-      _startTimer();
-      setState(() {});
+
+      if (resume && savedIndex < filteredQuestions.length) {
+        currentIndex = savedIndex;
+      }
     }
+    await _loadBestScore();
+    _startTimer();
+  }
+
+  Future<void> _saveProgress() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('saved_game_data', jsonEncode({
+      'game': 'quiz',
+      'era': selectedEra,
+      'historyType': selectedHistoryType,
+      'index': currentIndex + 1,
+    }));
   }
   void _showEmptyDialog() {
     showDialog(
@@ -105,6 +131,7 @@ class _QuizScreenState extends State<QuizScreen> {
 
     countdown?.cancel();
 
+    await _saveProgress();
     await Future.delayed(Duration(seconds: 1));
 
     if(currentIndex < filteredQuestions.length -1) {
@@ -119,7 +146,10 @@ class _QuizScreenState extends State<QuizScreen> {
       _showResultDiolog();
     }
   }
-  void _showResultDiolog() {
+  void _showResultDiolog() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('saved_game_data');
+
     showDialog(
         context: context,
         builder: (_) => AlertDialog(
@@ -252,307 +282,3 @@ class _QuizScreenState extends State<QuizScreen> {
   }
 }
 
-final List<QuizQuestion> allQuestions = [
-  // АНТИЧНОСТЬ — ИСТОРИЯ РОССИИ
-  QuizQuestion(
-    question: 'Какое племя считается предком славян?',
-    answers: ['Готы', 'Анты', 'Кельты', 'Саксы'],
-    correctIndex: 1,
-    era: 'Античность',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Какие племена обитали на территории будущей Руси?',
-    answers: ['Анты и склавины', 'Греки', 'Римляне', 'Финикийцы'],
-    correctIndex: 0,
-    era: 'Античность',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'С кем активно торговали восточные славяне?',
-    answers: ['С Римом', 'С Китаем', 'С Византией', 'С Египтом'],
-    correctIndex: 2,
-    era: 'Античность',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Какая религия преобладала у славян в античную эпоху?',
-    answers: ['Христианство', 'Язычество', 'Буддизм', 'Ислам'],
-    correctIndex: 1,
-    era: 'Античность',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Кто такие скифы?',
-    answers: ['Цари', 'Военные', 'Кочевники', 'Земледельцы'],
-    correctIndex: 2,
-    era: 'Античность',
-    historyType: 'История России',
-  ),
-
-  // АНТИЧНОСТЬ — МИРОВАЯ ИСТОРИЯ
-  QuizQuestion(
-    question: 'Где зародилась демократия?',
-    answers: ['Рим', 'Афины', 'Карфаген', 'Персия'],
-    correctIndex: 1,
-    era: 'Античность',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Как звали главного бога в Древнем Риме?',
-    answers: ['Зевс', 'Юпитер', 'Аполлон', 'Гермес'],
-    correctIndex: 1,
-    era: 'Античность',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Кто был учителем Александра Македонского?',
-    answers: ['Сократ', 'Аристотель', 'Платон', 'Гомер'],
-    correctIndex: 1,
-    era: 'Античность',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Какое событие произошло в 44 году до н.э.?',
-    answers: ['Разгром Карфагена', 'Гибель Цезаря', 'Падение Афин', 'Основание Рима'],
-    correctIndex: 1,
-    era: 'Античность',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Какая битва считается началом конца Римской республики?',
-    answers: ['При Акциуме', 'При Каннах', 'При Гавгамелах', 'При Филиппах'],
-    correctIndex: 0,
-    era: 'Античность',
-    historyType: 'Мировая история',
-  ),
-
-  // СРЕДНЕВЕКОВЬЕ — ИСТОРИЯ РОССИИ
-  QuizQuestion(
-    question: 'Кто правил при принятии христианства на Руси?',
-    answers: ['Олег', 'Владимир', 'Ярослав', 'Игорь'],
-    correctIndex: 1,
-    era: 'Средневековье',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Кто основал Москву?',
-    answers: ['Иван Калита', 'Юрий Долгорукий', 'Александр Невский', 'Даниил Московский'],
-    correctIndex: 1,
-    era: 'Средневековье',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Когда началось монголо-татарское вторжение?',
-    answers: ['1223', '1240', '1380', '1480'],
-    correctIndex: 1,
-    era: 'Средневековье',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Где произошло Ледовое побоище?',
-    answers: ['На Неве', 'На Чудском озере', 'На Волге', 'На Днепре'],
-    correctIndex: 1,
-    era: 'Средневековье',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Какой город стал центром объединения русских земель?',
-    answers: ['Новгород', 'Киев', 'Москва', 'Владимир'],
-    correctIndex: 2,
-    era: 'Средневековье',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Оборона чего была прорванна в 2006г до н.э ?',
-    answers: ['Дебага', 'Алёшина Александра', 'Охтинки', 'Ютуба'],
-    correctIndex: 2,
-    era: 'Средневековье',
-    historyType: 'История России',
-  ),
-
-  // СРЕДНЕВЕКОВЬЕ — МИРОВАЯ ИСТОРИЯ
-  QuizQuestion(
-    question: 'Кто начал Крестовые походы?',
-    answers: ['Папа Римский Урбан II', 'Карл Великий', 'Ричард Львиное Сердце', 'Саладин'],
-    correctIndex: 0,
-    era: 'Средневековье',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Какой город пал в 1453 году?',
-    answers: ['Рим', 'Константинополь', 'Париж', 'Афины'],
-    correctIndex: 1,
-    era: 'Средневековье',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Кто был основателем ислама?',
-    answers: ['Моисей', 'Иисус', 'Мухаммед', 'Будда'],
-    correctIndex: 2,
-    era: 'Средневековье',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'В каких странах происходила Столетняя война?',
-    answers: ['Англия и Франция', 'Испания и Португалия', 'Италия и Германия', 'Россия и Польша'],
-    correctIndex: 0,
-    era: 'Средневековье',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Как назывался главный враг рыцарей-крестоносцев?',
-    answers: ['Карфагеняне', 'Сарацины', 'Греки', 'Скифы'],
-    correctIndex: 1,
-    era: 'Средневековье',
-    historyType: 'Мировая история',
-  ),
-
-  // НОВОЕ ВРЕМЯ — ИСТОРИЯ РОССИИ
-  QuizQuestion(
-    question: 'Кто провёл реформу календаря и ввёл Новый год 1 января?',
-    answers: ['Пётр I', 'Иван Грозный', 'Николай I', 'Екатерина II'],
-    correctIndex: 0,
-    era: 'Новое время',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'В каком году началась Отечественная война?',
-    answers: ['1810', '1812', '1815', '1805'],
-    correctIndex: 1,
-    era: 'Новое время',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Как звали главного реформатора XIX века?',
-    answers: ['Павел I', 'Николай II', 'Александр II', 'Иван IV'],
-    correctIndex: 2,
-    era: 'Новое время',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Какое событие произошло в 1861 году?',
-    answers: ['Крепостное право отменено', 'Революция', 'Крымская война', 'Реформа образования'],
-    correctIndex: 0,
-    era: 'Новое время',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Кто такая Екатерина II?',
-    answers: ['Императрица', 'Министр', 'Генерал', 'Царица Египта'],
-    correctIndex: 0,
-    era: 'Новое время',
-    historyType: 'История России',
-  ),
-
-  // НОВОЕ ВРЕМЯ — МИРОВАЯ ИСТОРИЯ
-  QuizQuestion(
-    question: 'Кто открыл путь в Индию вокруг Африки?',
-    answers: ['Магеллан', 'Колумб', 'Васко да Гама', 'Кортес'],
-    correctIndex: 2,
-    era: 'Новое время',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Годы Войны за независимость в США?',
-    answers: ['1775–1783', '1812', '1492', '1861–1865'],
-    correctIndex: 0,
-    era: 'Новое время',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Кто был первым президентом США?',
-    answers: ['Линкольн', 'Вашингтон', 'Джефферсон', 'Адамс'],
-    correctIndex: 1,
-    era: 'Новое время',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Когда произошёл Наполеоновский поход в Россию?',
-    answers: ['1805', '1812', '1799', '1820'],
-    correctIndex: 1,
-    era: 'Новое время',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Великая французская революция - это?',
-    answers: ['Реформа', 'Период переворотов и реформ', 'Свержение монархии', 'Война с Англией'],
-    correctIndex: 2,
-    era: 'Новое время',
-    historyType: 'Мировая история',
-  ),
-
-  // СОВРЕМЕННОСТЬ — ИСТОРИЯ РОССИИ
-  QuizQuestion(
-    question: 'Кто проводил политику перестройки?',
-    answers: ['Брежнев', 'Горбачёв', 'Путин', 'Ельцин'],
-    correctIndex: 1,
-    era: 'Современность',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'В каком году была Программа приватизации?',
-    answers: ['1991', '1992', '1995', '2000'],
-    correctIndex: 1,
-    era: 'Современность',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Что произошло в октябре 1993 года?',
-    answers: ['Распад СССР', 'Вооружённый конфликт в Москве', 'Выборы Путина', 'Война в Чечне'],
-    correctIndex: 1,
-    era: 'Современность',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Как называлась военная операция России 1994–1996 гг.?',
-    answers: ['Югославия', 'Чечня', 'Афганистан', 'Сирия'],
-    correctIndex: 1,
-    era: 'Современность',
-    historyType: 'История России',
-  ),
-  QuizQuestion(
-    question: 'Кто стал президентом в 2000 году?',
-    answers: ['Ельцин', 'Медведев', 'Путин', 'Горбачёв'],
-    correctIndex: 2,
-    era: 'Современность',
-    historyType: 'История России',
-  ),
-
-  // СОВРЕМЕННОСТЬ — МИРОВАЯ ИСТОРИЯ
-  QuizQuestion(
-    question: 'Когда закончилась Вторая мировая война?',
-    answers: ['1941', '1945', '1939', '1943'],
-    correctIndex: 1,
-    era: 'Современность',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Кто был лидером Германии во время Второй мировой?',
-    answers: ['Гитлер', 'Сталин', 'Черчилль', 'Рузвельт'],
-    correctIndex: 0,
-    era: 'Современность',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Что такое НАТО?',
-    answers: ['Военный союз', 'Экономическое объединение', 'Фонд', 'Научная организация'],
-    correctIndex: 0,
-    era: 'Современность',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Какой из представленных городов был разрушен атомной бомбой?',
-    answers: ['Хиросима', 'Токио', 'Пекин', 'Нагоя'],
-    correctIndex: 0,
-    era: 'Современность',
-    historyType: 'Мировая история',
-  ),
-  QuizQuestion(
-    question: 'Когда распался СССР?',
-    answers: ['1989', '1990', '1991', '1992'],
-    correctIndex: 2,
-    era: 'Современность',
-    historyType: 'Мировая история',
-  ),
-];
